0cf2c6766f3e18a95778117272812c04b7787cf3
Charly Wargnier <cwar05@gmail.com>
2022-03-10 13:26:44 +0000

    reverted to "streamlit", "dash", etc... as numpy was throwing an error

--- app.py
+++ app.py
@@ -230,8 +230,8 @@ def main():
     select_packages = st.multiselect(
         "Select Python packages to compare",
         package_names,
-        default=["numpy", "pandas"],
-        # default=["streamlit", "dash", "panel", "voila"],
+        # default=["numpy", "pandas"],
+        default=["streamlit", "dash", "panel", "voila"],
         help=instructions,
     )
 
